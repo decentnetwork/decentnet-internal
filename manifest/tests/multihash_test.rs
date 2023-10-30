0b5dc5185c8982102d4359b4e22a33d47f48148d
canewsin <canews.in@gmail.com>
2023-10-31 00:20:42 +0530

    tests: update multihash tests

--- manifest/tests/multihash_test.rs
+++ manifest/tests/multihash_test.rs
@@ -1,18 +1,27 @@
-use cid::Cid;
-use multihash::{Code, MultihashDigest};
+use cid::{
+    multihash::{Code, MultihashDigest},
+    Cid,
+};
+use multihash::Multihash;
 
 #[test]
 pub fn test_multihash() {
-    let data = b"Hello, world!";
+    let data = b"Hello 1NGmnrtExYz4TApULNuiv4VpWS3rjKvnYX!";
 
-    let digest = Code::Sha2_256.digest(data);
+    let digest = Code::Blake3_256.digest(data);
     let cid = Cid::new_v1(0x55, digest);
 
     // Generate a multihash from the digest
     let multihash = cid.to_bytes();
 
+    let hash = Multihash::<64>::from_bytes(&cid.hash().to_bytes()).unwrap();
+    println!("Multihash: {:x?}\n", &hash.to_bytes());
+
     let cid2 = Cid::try_from(multihash).unwrap();
-    cid2.to_string();
+    assert_eq!(cid, cid2);
+    println!("Cid: {:?}", cid2.version());
+    let cid_str = cid2.to_string();
+    println!("Cid: {}", cid_str);
 
     // Print the multihash as hex string
     let multihash_hex = cid
